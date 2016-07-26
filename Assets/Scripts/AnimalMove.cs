ae44a293096f36e3d68b3c5715b7216bc5478641
Andrew Moran <andrewmo@mit.edu>
2016-07-25 17:34:14 -0700

    Initialized MVP Scene 2

---
+++ Assets/Scripts/AnimalMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalMove : MonoBehaviour {
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    public GameObject target;
+
+    void Awake()
+    {
+        startPos = gameObject.transform.position;
+        endPos = target.transform.position;
+    }
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("position", endPos, "easeType", "easeInOutExpo", "loopType", "none", "delay", 1.0f));
+    }
+
+}

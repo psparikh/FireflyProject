7913ab1fdc4d92f1b1bd9b3eab7eacb9a853f25e
Andrew Moran <andrewmo@mit.edu>
2016-07-26 12:05:49 -0700

    Adding three targets for slinghot

---
+++ Assets/Scripts/Mechanics/Destroy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour
+{
+
+    public bool triggerEvent = false;
+
+    void Start()
+    {
+        //gameObject.GetComponent<Rigidbody>().AddForce(0, 0, -200);
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "bullet")
+        {
+            gameObject.AddComponent<TriangleExplosion>();
+            StartCoroutine(gameObject.GetComponent<TriangleExplosion>().SplitMesh(true));
+            //col.gameObject.GetComponent<Collide>().device.TriggerHapticPulse(700);
+        }
+    }
+
+    void Update()
+    {
+        if (triggerEvent)
+        {
+            gameObject.AddComponent<TriangleExplosion>();
+            StartCoroutine(gameObject.GetComponent<TriangleExplosion>().SplitMesh(true));
+            //col.gameObject.GetComponent<Collide>().device.TriggerHapticPulse(700);
+            triggerEvent = false;
+        }
+    }
+}

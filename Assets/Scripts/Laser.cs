81f9f459f578b54dd028b3850727bb53f0fd7c65
Andrew Moran <andrewmo@mit.edu>
2016-07-27 14:12:19 -0700

    MVP Scene 4 Update

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour
+{
+
+    LineRenderer lr;
+
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetColor(Color color)
+    {
+        lr.SetColors(color, color);
+    }
+
+    public void SetWaypoints(Vector3[] waypoints)
+    {
+
+        lr.SetVertexCount(waypoints.Length);
+        lr.SetPositions(waypoints);
+    }
+}
---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class Reticle : MonoBehaviour
+{
+
+    private Light halo;
+
+    // Use this for initialization
+    void Start()
+    {
+        halo = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetColor(Color color)
+    {
+
+        halo.color = color;
+    }
+
+}

a333d392c7d6cd878f9b219cbca310db1869009f
Priyam Parikh <psparikh@usc.edu>
2016-08-02 10:31:37 -0700

    Adding Lightbox and many updates to Scene 3

---
+++ Assets/Scripts/Actions/TrainMoveCinema.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainMoveCinema : MonoBehaviour {
+
+    public ParticleSystem system;
+
+    public void Move()
+    {
+        iTween.MoveBy(gameObject, iTween.Hash("z", -150, "easeType", "easeOutCubic", "time", 20, "delay", 0));
+    }
+
+
+
+    public void StartRain()
+    {
+
+        ParticleSystem.EmissionModule emissionMod = system.emission;
+        ParticleSystem.MinMaxCurve minMax = new ParticleSystem.MinMaxCurve();
+        minMax.constantMax = 2000;
+        emissionMod.rate = minMax;
+
+    }
+}
--- Assets/Scripts/Mechanics/ChangeFirefly.cs
+++ Assets/Scripts/Mechanics/ChangeFirefly.cs
@@ -16,19 +16,7 @@ public class ChangeFirefly : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         device = SteamVR_Controller.Input((int)trackedObject.index);
-       /* if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
-        {
-            //device.TriggerHapticPulse(pulsePower);
-            ParticleSystem.InheritVelocityModule inheritVel = firefly.inheritVelocity;
-            inheritVel.mode = ParticleSystemInheritVelocityMode.Current;
 
-        }
-        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
-        {
-            ParticleSystem.InheritVelocityModule inheritVel = firefly.inheritVelocity;
-            inheritVel.mode = ParticleSystemInheritVelocityMode.Initial;
-
-        }*/
         if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
         {
             firefly.Play();
@@ -37,12 +25,12 @@ public class ChangeFirefly : MonoBehaviour {
             minMax.constantMax = 0;
             emissionMod.rate = minMax;
         }
-        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger) || Input.GetKeyDown(KeyCode.Space))
         {
             firefly.Play();
             ParticleSystem.EmissionModule emissionMod = firefly.emission;
             ParticleSystem.MinMaxCurve minMax = new ParticleSystem.MinMaxCurve();
-            minMax.constantMax = 1000;
+            minMax.constantMax = 100;
             emissionMod.rate = minMax;
 
         }
